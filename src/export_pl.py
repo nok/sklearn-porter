ab9ff3ffe253faccf73dc61635da60cc83cdfa73
Darius Morawiec <mail@nok.onl>
2016-06-19 23:39:03 +0200

    add basic command line functionality

--- src/export_pl.py
+++ src/export_pl.py
@@ -68,7 +68,29 @@ def export_pl(clf, method_name='predict'):
         return source
 
 def main():
-    pass
+    # from sklearn.externals import joblib
+    # joblib.dump(clf, 'filename.pkl')
+    import sys
+    if len(sys.argv) == 3:
+        input_file = str(sys.argv[1])
+        output_file = str(sys.argv[2])
+        if input_file.endswith('.pkl') and output_file.endswith('.java'):
+            from sklearn.externals import joblib
+            with open(output_file, 'w') as file:
+                clf = joblib.load(input_file)
+                java_src = ('class {0} {{ \n'
+                            '    public static {1} \n'
+                            '    public static void main(String[] args) {{ \n'
+                            '        if (args.length == {2}) {{ \n'
+                            '            float[] atts = new float[args.length]; \n'
+                            '            for (int i = 0; i < args.length; i++) {{ \n'
+                            '                atts[i] = Float.parseFloat(args[i]); \n'
+                            '            }} \n'
+                            '            System.out.println({0}.predict(atts)); \n'
+                            '        }} \n'
+                            '    }} \n'
+                            '}}').format(output_file.split('.')[0].title(), export_pl(clf), clf.n_features_)
+                file.write(java_src)
 
 if __name__ == '__main__':
     main()
