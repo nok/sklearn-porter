350e66457b65e91483a2c7c662b5252c06d8994f
Darius Morawiec <mail@nok.onl>
2016-07-10 20:43:29 +0200

    add initial adaboost implementation

--- examples/regression/adaboost_predict.py
+++ examples/regression/adaboost_predict.py
@@ -2,15 +2,255 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-# from onl.nok.sklearn.export.Export import Export
+from onl.nok.sklearn.export.Export import Export
 
 iris = load_iris()
 base_estimator = DecisionTreeClassifier(max_depth=4)
-clf = AdaBoostClassifier(base_estimator=base_estimator, n_estimators=100)
+clf = AdaBoostClassifier(base_estimator=base_estimator, n_estimators=100, random_state=1)
 clf.fit(iris.data, iris.target)
 
 # Cheese!
 
-# TODO: Implement and add export
-# trees = Export.export(clf)
-# print(trees)
+trees = Export.export(clf)
+print(trees)
+
+# class Tmp {
+#     public static float[] predict_000(float[] atts) {
+#         int n_classes = 3;
+#         float[] classes = new float[n_classes];
+#
+#         if (atts[3] <= 0.800000f) {
+#             classes[0] = 0.333333f;
+#             classes[1] = 0.000000f;
+#             classes[2] = 0.000000f;
+#         } else {
+#             if (atts[3] <= 1.750000f) {
+#                 if (atts[2] <= 4.950000f) {
+#                     if (atts[3] <= 1.650000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.313333f;
+#                         classes[2] = 0.000000f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.006667f;
+#                     }
+#                 } else {
+#                     if (atts[3] <= 1.550000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.020000f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.013333f;
+#                         classes[2] = 0.006667f;
+#                     }
+#                 }
+#             } else {
+#                 if (atts[2] <= 4.850000f) {
+#                     if (atts[0] <= 5.950000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.006667f;
+#                         classes[2] = 0.000000f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.013333f;
+#                     }
+#                 } else {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000000f;
+#                     classes[2] = 0.286667f;
+#                 }
+#             }
+#         }
+#
+#         return classes;
+#     }
+#
+#     public static float[] predict_001(float[] atts) {
+#         int n_classes = 3;
+#         float[] classes = new float[n_classes];
+#
+#         if (atts[2] <= 5.150000f) {
+#             if (atts[2] <= 2.450000f) {
+#                 classes[0] = 0.000083f;
+#                 classes[1] = 0.000000f;
+#                 classes[2] = 0.000000f;
+#             } else {
+#                 if (atts[3] <= 1.750000f) {
+#                     if (atts[0] <= 4.950000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000002f;
+#                         classes[2] = 0.000002f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.499954f;
+#                         classes[2] = 0.000003f;
+#                     }
+#                 } else {
+#                     if (atts[1] <= 3.150000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.000020f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000002f;
+#                         classes[2] = 0.000002f;
+#                     }
+#                 }
+#             }
+#         } else {
+#             classes[0] = 0.000000f;
+#             classes[1] = 0.000000f;
+#             classes[2] = 0.499933f;
+#         }
+#
+#         return classes;
+#     }
+#
+#     public static float[] predict_002(float[] atts) {
+#         int n_classes = 3;
+#         float[] classes = new float[n_classes];
+#
+#         if (atts[3] <= 1.550000f) {
+#             if (atts[2] <= 4.950000f) {
+#                 if (atts[3] <= 0.800000f) {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000000f;
+#                     classes[2] = 0.000000f;
+#                 } else {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000185f;
+#                     classes[2] = 0.000000f;
+#                 }
+#             } else {
+#                 classes[0] = 0.000000f;
+#                 classes[1] = 0.000000f;
+#                 classes[2] = 0.499697f;
+#             }
+#         } else {
+#             if (atts[2] <= 5.150000f) {
+#                 if (atts[3] <= 1.850000f) {
+#                     if (atts[1] <= 2.600000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.000111f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.499735f;
+#                         classes[2] = 0.000000f;
+#                     }
+#                 } else {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000000f;
+#                     classes[2] = 0.000111f;
+#                 }
+#             } else {
+#                 classes[0] = 0.000000f;
+#                 classes[1] = 0.000000f;
+#                 classes[2] = 0.000161f;
+#             }
+#         }
+#
+#         return classes;
+#     }
+#
+#     public static float[] predict_003(float[] atts) {
+#         int n_classes = 3;
+#         float[] classes = new float[n_classes];
+#
+#         if (atts[3] <= 1.750000f) {
+#             if (atts[3] <= 1.550000f) {
+#                 if (atts[2] <= 4.950000f) {
+#                     if (atts[2] <= 2.450000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.000000f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000001f;
+#                         classes[2] = 0.000000f;
+#                     }
+#                 } else {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000000f;
+#                     classes[2] = 0.001727f;
+#                 }
+#             } else {
+#                 if (atts[2] <= 5.450000f) {
+#                     if (atts[1] <= 2.600000f) {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.000000f;
+#                         classes[2] = 0.000000f;
+#                     } else {
+#                         classes[0] = 0.000000f;
+#                         classes[1] = 0.499024f;
+#                         classes[2] = 0.000000f;
+#                     }
+#                 } else {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000000f;
+#                     classes[2] = 0.000001f;
+#                 }
+#             }
+#         } else {
+#             if (atts[1] <= 3.150000f) {
+#                 classes[0] = 0.000000f;
+#                 classes[1] = 0.000000f;
+#                 classes[2] = 0.499136f;
+#             } else {
+#                 if (atts[2] <= 4.950000f) {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000111f;
+#                     classes[2] = 0.000000f;
+#                 } else {
+#                     classes[0] = 0.000000f;
+#                     classes[1] = 0.000000f;
+#                     classes[2] = 0.000000f;
+#                 }
+#             }
+#         }
+#
+#         return classes;
+#     }
+#
+#     public static int predict(float[] atts) {
+#         int n_estimators = 4;
+#         float[][] preds = new float[n_estimators][];
+#         preds[0] = Tmp.predict_000(atts);
+#         preds[1] = Tmp.predict_001(atts);
+#         preds[2] = Tmp.predict_002(atts);
+#         preds[3] = Tmp.predict_003(atts);
+#         int n_classes = 3;
+#         float[] classes = new float[n_classes];
+#         for (int i = 1; i < n_estimators; i++) {
+#             for (int j = 0; j < n_classes; j++) {
+#                 classes[j] += preds[i][j];
+#             }
+#         }
+#
+#         int idx = 0;
+#         float val = classes[0];
+#         for (int i = 1; i < n_classes; i++) {
+#             if (classes[i] > val) {
+#                 idx = i;
+#             }
+#         }
+#         return idx;
+#     }
+#
+#     public static void main(String[] args) {
+#
+#         int pred = Tmp.predict(new float[]{0.3f, 0.5f, 0f, 0.5f});
+#         System.out.println(pred);
+#
+#         if (args.length == 4) {
+#             float[] atts = new float[args.length];
+#             for (int i = 0; i < args.length; i++) {
+#                 atts[i] = Float.parseFloat(args[i]);
+#             }
+#             System.out.println(Tmp.predict(atts));
+#         }
+#     }
+# }
--- onl/nok/sklearn/export/classifier/AdaBoostClassifier.py
+++ onl/nok/sklearn/export/classifier/AdaBoostClassifier.py
@@ -1,4 +1,8 @@
+import numpy as np
+import sklearn
+
 from onl.nok.sklearn.export.classifier.Classifier import Classifier
+from onl.nok.sklearn.export.classifier.DecisionTreeClassifier import DecisionTreeClassifier
 
 
 class AdaBoostClassifier(Classifier):
@@ -21,5 +25,138 @@ class AdaBoostClassifier(Classifier):
 
     @staticmethod
     def export(model, method_name='predict', class_name="Tmp"):
-        # TODO: Implement export
-        pass
+        print("Ada.export()")
+        if AdaBoostClassifier.is_supported_method(method_name):
+            if method_name == 'predict':
+                return AdaBoostClassifier.predict(model, class_name=class_name)
+        return False
+
+
+    @staticmethod
+    def predict(model, class_name='Tmp'):
+        print("Ada.predict()")
+        method_name = 'predict'
+
+        if model.algorithm not in ('SAMME', 'SAMME.R'):
+            # raise ValueError("algorithm %s is not supported" % model.algorithm)
+            return False
+
+        # Check type of base estimators:
+        if not isinstance(model.base_estimator, sklearn.tree.tree.DecisionTreeClassifier):
+            return False
+
+        # Check number of base estimators:
+        if not model.n_estimators > 0:
+            return False
+
+        # TODO: Use local variables
+        # n_features = model.estimators_[0].n_features_
+        # n_classes = model.n_classes_
+
+        function_names = []
+        functions = []
+        n_estimators = 0
+        for idx in range(model.n_estimators):
+            weight = float(model.estimator_weights_[idx])
+            if weight > 0:
+                n_estimators += 1
+                id = ("{0:0" + str(len(str(model.n_estimators))) + "d}").format(idx)
+                function_name_with_id = method_name + "_" + id
+                function_names.append(function_name_with_id)
+                functions.append(AdaBoostClassifier._create_trees(
+                    model.estimators_[idx], function_name_with_id))
+
+        str_tree_calls = "\n".join(['preds[{0}] = {1}.{2}(atts);'.format(int(i), class_name, function_name) for i, function_name in enumerate(function_names)])
+        str_trees = '\n'.join(functions)
+        str_method = AdaBoostClassifier._create_method(model, method_name, str_tree_calls, n_estimators)
+        str_class = AdaBoostClassifier._create_class(model, class_name, str_trees, str_method)
+        return str_class
+
+
+    @staticmethod
+    def _create_trees(model, method_name):
+        method_name = str(method_name)
+        n_features = model.n_features_
+        n_classes = model.n_classes_
+
+        def _recurse(left, right, threshold, value, features, node, depth):
+            out = ''
+            indent = '\n' + '    ' * depth
+            if threshold[node] != -2.:
+                out += indent + 'if (atts[{0}] <= {1:.6f}f) {{'.format(features[node], threshold[node])
+                if left[node] != -1.:
+                    out += _recurse(left, right, threshold, value, features, left[node], depth + 1)
+                out += indent + '} else {'
+                if right[node] != -1.:
+                    out += _recurse(left, right, threshold, value, features, right[node], depth + 1)
+                out += indent + '}'
+            else:
+                out += ';'.join(
+                    [indent + 'classes[{0}] = {1:.6f}f'.format(i, v) for i, v in enumerate(value[node][0])]) + ';'
+            return out
+
+        features = [[str(idx) for idx in range(n_features)][i] for i in model.tree_.feature]
+
+        conditions = _recurse(
+            model.tree_.children_left,
+            model.tree_.children_right,
+            model.tree_.threshold,
+            model.tree_.value, features, 0, 1)
+
+        out = (
+            'public static float[] {0}(float[] atts) {{ \n'
+            '    int n_classes = {1}; \n'
+            '    float[] classes = new float[n_classes]; \n'
+            '    {2} \n\n'
+            '    return classes; \n'
+            '}}'
+        ).format(method_name, n_classes, conditions)  # -> {0}, {1}, {2}
+        return str(out)
+
+
+    @staticmethod
+    def _create_method(model, method_name, str_trees, n_estimators):
+        print("Ada._create_method()")
+        n_classes = model.n_classes_
+        out = (
+            'public static int {0}(float[] atts) {{ \n'
+            '    int n_estimators = {1}; \n'
+            '    float[][] preds = new float[n_estimators][]; \n'
+            '    {2} \n'
+            '    int n_classes = {3}; \n'
+            '    float[] classes = new float[n_classes]; \n'
+            '    for (int i = 1; i < n_estimators; i++) {{ \n'
+            '        for (int j = 0; j < n_classes; j++) {{ \n'
+            '            classes[j] += preds[i][j]; \n'
+            '        }} \n'
+            '    }} \n\n'
+            '    int idx = 0; \n'
+            '    float val = classes[0]; \n'
+            '    for (int i = 1; i < n_classes; i++) {{ \n'
+            '        if (classes[i] > val) {{ \n'
+            '            idx = i; \n'
+            '        }} \n'
+            '    }} \n'
+            '    return idx; \n'
+            '}}').format(method_name, n_estimators, str_trees, n_classes)
+        return str(out)
+
+
+    @staticmethod
+    def _create_class(model, class_name, str_trees, str_method):
+        n_features = model.estimators_[0].n_features_
+        out = (
+            'class {0} {{ \n'
+            '    {1} \n'
+            '    {2} \n'
+            '    public static void main(String[] args) {{ \n'
+            '        if (args.length == {3}) {{ \n'
+            '            float[] atts = new float[args.length]; \n'
+            '            for (int i = 0; i < args.length; i++) {{ \n'
+            '                atts[i] = Float.parseFloat(args[i]); \n'
+            '            }} \n'
+            '            System.out.println({0}.predict(atts)); \n'
+            '        }} \n'
+            '    }} \n'
+            '}}').format(class_name, str_trees, str_method, n_features)
+        return str(out)
