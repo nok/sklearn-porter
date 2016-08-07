7d31535dd0f5b6d673ecebb3b81ce7e62fc1d3e8
Darius Morawiec <darius.morawiec@nok.onl>
2016-08-07 17:08:08 +0200

    change or simplify env name

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 conda config --add channels conda-forge
-conda env create -n sklearn.tree.model.export python=2 -f environment.yml
-source activate sklearn.tree.model.export
+conda env create -n sklearn-porting python=2 -f environment.yml
+source activate sklearn-porting
