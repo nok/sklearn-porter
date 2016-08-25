6d29816ecc81d61ea5caebb77d136bc123c9c0e5
Darius Morawiec <darius.morawiec@nok.onl>
2016-08-26 01:00:42 +0200

    add docstrings

--- onl/nok/sklearn/classifier/Classifier.py
+++ onl/nok/sklearn/classifier/Classifier.py
@@ -1,10 +1,18 @@
 
 
 class Classifier(object):
+
     def __init__(self, language='java', method_name='predict', class_name='Tmp'):
         self.language = language
         self.method_name = method_name
         self.class_name = class_name
 
     def port(self, model):
+        """Port a trained model in the syntax of a specific programming language.
+
+        Parameters
+        ----------
+        :param model : scikit-learn model object
+            An instance of a trained model (e.g. DecisionTreeClassifier).
+        """
         self.model = model
