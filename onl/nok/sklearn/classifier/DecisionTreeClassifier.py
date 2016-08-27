b39be494e1d6820d713ce45141cd14f204f19825
Darius Morawiec <darius.morawiec@nok.onl>
2016-08-27 13:13:22 +0200

    add javascript version

--- onl/nok/sklearn/classifier/DecisionTreeClassifier.py
+++ onl/nok/sklearn/classifier/DecisionTreeClassifier.py
@@ -36,14 +36,13 @@ class DecisionTreeClassifier(Classifier):
         :return: out : string
             The ported predict method.
         """
-        str_method = self.create_method()
         str_class = self.create_class()
+        str_method = self.create_method()
         out = str_class.format(str_method)
         return out
 
-    @staticmethod
-    def parse(L, R, T, value, features, node, depth):
-        """Parse and port the model data.
+    def create_tree(self, L, R, T, value, features, node, depth):
+        """Parse and port the model tree.
 
         Parameters
         ----------
@@ -71,12 +70,16 @@ class DecisionTreeClassifier(Classifier):
         indent = '\n' + '    ' * depth
         # @formatter:off
         if T[node] != -2.:
-            out += indent + 'if (atts[{0}] <= {1:.6f}f) {{'.format(features[node], T[node])
+            template = {
+                'java': ('if (atts[{0}] <= {1:.6f}f) {{'),
+                'js': ('if (atts[{0}] <= {1:.6f}) {{')
+            }
+            out += indent + template[self.language].format(features[node], T[node])
             if L[node] != -1.:
-                out += DecisionTreeClassifier.parse(L, R, T, value, features, L[node], depth + 1)
+                out += self.create_tree(L, R, T, value, features, L[node], depth + 1)
             out += indent + '} else {'
             if R[node] != -1.:
-                out += DecisionTreeClassifier.parse(L, R, T, value, features, R[node], depth + 1)
+                out += self.create_tree(L, R, T, value, features, R[node], depth + 1)
             out += indent + '}'
         else:
             for idx, val in enumerate(value[node][0]):
@@ -86,36 +89,65 @@ class DecisionTreeClassifier(Classifier):
         return out
 
     def create_method(self):
-        """Build the model method.
+        """Build the model method or function.
 
         Returns
         -------
         :return out : string
             The built method as string.
         """
-        features = [[str(idx) for idx in range(self.n_features)][i] for i in self.model.tree_.feature]
-        conditions = DecisionTreeClassifier.parse(self.model.tree_.children_left,
-                                                  self.model.tree_.children_right,
-                                                  self.model.tree_.threshold,
-                                                  self.model.tree_.value, features, 0, 1)
-        out = (
-            # @formatter:off
-            'public static int {0}(float[] atts) {{ \n'
-            '    int n_classes = {1}; \n'
-            '    int[] classes = new int[n_classes]; \n'
-            '    {2} \n\n'
-            '    int idx = 0; \n'
-            '    int val = classes[0]; \n'
-            '    for (int i = 1; i < n_classes; i++) {{ \n'
-            '        if (classes[i] > val) {{ \n'
-            '            idx = i; \n'
-            '            val = classes[i]; \n'
-            '        }} \n'
-            '    }} \n'
-            '    return idx; \n'
-            '}}'
-            # @formatter:on
-        ).format(self.method_name, self.n_classes, conditions)  # -> {0}, {1}, {2}
+        feature_indices = []
+        for i in self.model.tree_.feature:
+            feature_indices.append([str(j) for j in range(self.n_features)][i])
+
+        conditions = self.create_tree(
+            self.model.tree_.children_left,
+            self.model.tree_.children_right,
+            self.model.tree_.threshold,
+            self.model.tree_.value, feature_indices, 0, 1)
+
+        template = {
+            'java': (
+                # @formatter:off
+                'public static int {0}(float[] atts) {{ \n'
+                '    if (atts.length != {1}) {{ return -1; }}; \n\n'
+                '    int[] classes = new int[{2}]; \n'
+                '    {3} \n\n'
+                '    int idx = 0; \n'
+                '    int val = classes[0]; \n'
+                '    for (int i = 1; i < {2}; i++) {{ \n'
+                '        if (classes[i] > val) {{ \n'
+                '            idx = i; \n'
+                '            val = classes[i]; \n'
+                '        }} \n'
+                '    }} \n'
+                '    return idx; \n'
+                '}}'
+                # @formatter:on
+            ),
+            'js': (
+                # @formatter:off
+                'var {0} = function(atts) {{ \n'
+                '    if (atts.length != {1}) {{ return -1; }}; \n'
+                '    var classes = new Array({2}); \n'
+                '    {3} \n\n'
+                '    var idx = 0, val = classes[0]; \n'
+                '    for (var i = 1; i < {2}; i++) {{ \n'
+                '        if (classes[i] > val) {{ \n'
+                '            idx = i; \n'
+                '            val = classes[i]; \n'
+                '        }} \n'
+                '    }} \n'
+                '    return idx; \n'
+                '}};'
+                # @formatter:on
+            )
+        }
+        out = template[self.language].format(
+            self.method_name,
+            self.n_features,
+            self.n_classes,
+            conditions)
         return out
 
     def create_class(self):
@@ -126,20 +158,27 @@ class DecisionTreeClassifier(Classifier):
         :return out : string
             The built class as string.
         """
-        out = (
-            # @formatter:off
-            'class {0} {{{{ \n'
-            '    {{0}} \n'
-            '    public static void main(String[] args) {{{{ \n'
-            '        if (args.length == {1}) {{{{ \n'
-            '            float[] atts = new float[args.length]; \n'
-            '            for (int i = 0; i < args.length; i++) {{{{ \n'
-            '                atts[i] = Float.parseFloat(args[i]); \n'
-            '            }}}} \n'
-            '            System.out.println({0}.predict(atts)); \n'
-            '        }}}} \n'
-            '    }}}} \n'
-            '}}}}'
-            # @formatter:on
-        ).format(self.class_name, self.n_features)  # -> {0}, {1}
+        template = {
+            'java': (
+                # @formatter:off
+                'class {0} {{{{ \n'
+                '    {{0}} \n'
+                '    public static void main(String[] args) {{{{ \n'
+                '        if (args.length == {1}) {{{{ \n'
+                '            float[] atts = new float[args.length]; \n'
+                '            for (int i = 0, l = args.length; i < l; i++) {{{{ \n'
+                '                atts[i] = Float.parseFloat(args[i]); \n'
+                '            }}}} \n'
+                '            System.out.println({0}.predict(atts)); \n'
+                '        }}}} \n'
+                '    }}}} \n'
+                '}}}}'
+                # @formatter:on
+            ),
+            # Just insert the single function:
+            'js': ('{{0}}')
+        }
+        out = template[self.language].format(
+            self.class_name,
+            self.n_features)
         return out
