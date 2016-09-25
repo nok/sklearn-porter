404bea1b0d43bba457db9c1c1d64558924a68448
Darius Morawiec <darius.morawiec@nok.onl>
2016-09-25 13:23:15 +0200

    separate build from language templates

--- onl/nok/sklearn/classifier/AdaBoostClassifier.py
+++ onl/nok/sklearn/classifier/AdaBoostClassifier.py
@@ -16,217 +16,25 @@ class AdaBoostClassifier(Classifier):
         'predict': ['java', 'js']
     }
 
-
-    def __init__(self, language='java', method_name='predict', class_name='Tmp'):
-        super(self.__class__, self).__init__(language, method_name, class_name)
-
-
-    def port(self, model):
-        """Port a trained model to the syntax of a chosen programming language.
-
-        Parameters
-        ----------
-        :param model : AdaBoostClassifier
-            An instance of a trained AdaBoostClassifier model.
-        """
-
-        # Check the used algorithm type:
-        if model.algorithm not in ('SAMME.R'):
-            msg = 'The classifier does not support the given algorithm %s.'
-            raise ValueError(msg, model.algorithm)
-
-        # Check type of base estimators:
-        if not isinstance(model.base_estimator, sklearn.tree.tree.DecisionTreeClassifier):
-            msg = 'The classifier does not support the given base estimator %s.'
-            raise ValueError(msg, model.base_estimator)
-
-        # Check number of base estimators:
-        if not model.n_estimators > 0:
-            msg = 'The classifier has not any base estimators.'
-            raise ValueError(msg)
-
-        self.model = model
-        self.n_classes = model.n_classes_
-
-        self.models = []
-        self.weights = []
-        self.n_estimators = 0
-        for idx in range(self.model.n_estimators):
-            weight = self.model.estimator_weights_[idx]
-            if weight > 0:
-                self.models.append(self.model.estimators_[idx])
-                self.weights.append(self.model.estimator_weights_[idx])
-                self.n_estimators += 1
-                self.n_features = self.model.estimators_[idx].n_features_
-
-        if self.method_name == 'predict':
-            return self.predict()
-
-
-    def predict(self):
-        """Port the predict method.
-
-        Returns
-        -------
-        :return: out : string
-            The ported predict method.
-        """
-        str_class = self.create_class()
-        str_methods = self.create_methods()
-        out = str_class.format(str_methods)
-        return out
-
-
-    def parse_tree(self, L, R, T, value, features, node, depth):
-        """Port the structure of the model.
-
-        Parameters
-        ----------
-        :param L : object
-            The left children node.
-        :param R : object
-            The left children node.
-        :param T : object
-            The decision threshold.
-        :param value : object
-            The label or class.
-        :param features : object
-            The feature values.
-        :param node : int
-            The current node.
-        :param depth : int
-            The tree depth.
-
-        Returns
-        -------
-        :return : string
-            The ported structure of the tree model.
-        """
-        out = ''
-        indent = '\n' + '    ' * depth
-
-        if T[node] != -2.:
-
-            # @formatter:off
-            template = {
-                'java': ('if (atts[{0}] <= {1}) {{'),
-                'js': ('if (atts[{0}] <= {1}) {{')
-            }
-            # @formatter:on
-
-            out += indent + template[self.language].format(features[node], repr(T[node]))
-            if L[node] != -1.:
-                out += self.parse_tree(L, R, T, value, features, L[node], depth + 1)
-            out += indent + '} else {'
-            if R[node] != -1.:
-                out += self.parse_tree(L, R, T, value, features, R[node], depth + 1)
-            out += indent + '}'
-        else:
-            classes = []
-
-            # @formatter:off
-            template = {
-                'java': (indent + 'classes[{0}] = {1}'),
-                'js': (indent + 'classes[{0}] = {1}')
-            }
-            # @formatter:on
-
-            for idx, val in enumerate(value[node][0]):
-                classes.append(template[self.language].format(idx, repr(val)))
-            out += ';'.join(classes) + ';'
-        return out
-
-
-    def create_tree(self, model_index, model):
-        """Port a method for a single tree.
-
-        Parameters
-        ----------
-        :param model_index : int
-            The model index.
-        :param model : DecisionTreeClassifier
-            The model.
-
-        Returns
-        -------
-        :return : string
-            The created method.
-        """
-        feature_indices = []
-        for idx in model.tree_.feature:
-            feature_indices.append([str(jdx) for jdx in range(model.n_features_)][idx])
-
-        tree_branches = self.parse_tree(
-            model.tree_.children_left,
-            model.tree_.children_right,
-            model.tree_.threshold,
-            model.tree_.value,
-            feature_indices, 0, 1)
-
-        # @formatter:off
-        template = {
-            'java': (
+    # @formatter:off
+    TEMPLATE = {
+        'java': {
+            'if':       ('{0}if (atts[{1}] <= {2}) {{'),
+            'else':     ('{0}}} else {{'),
+            'endif':    ('{0}}}'),
+            'arr':      ('{0}classes[{1}] = {2}'),
+            'join':     ('; '),
+            'single_method': (
                 'public static double[] {1}_{0}(float[] atts) {{ \n'
                 '    double[] classes = new double[{2}];'
                 '    {3} \n\n'
                 '    return classes; \n'
                 '}}'
             ),
-            'js': (
-                'var {1}_{0} = function(atts) {{ \n'
-                '    var classes = new Array({2});'
-                '    {3} \n\n'
-                '    return classes; \n'
-                '}};'
-            )
-        }
-        # @formatter:on
-
-        out = template[self.language].format(
-            str(model_index),
-            self.method_name,
-            self.n_classes,
-            tree_branches)
-        return out
-
-
-    def create_methods(self):
-        """Build the model methods or functions.
-
-        Returns
-        -------
-        :return out : string
-            The built methods as merged string.
-        """
-        # Generate method or function names:
-        fn_names = []
-
-        # @formatter:off
-        template = {
-            'java': (
+            'method_calls': (
                 'preds[{0}] = {1}.{2}(atts);'
             ),
-            'js': (
-                'preds[{0}] = {2}(atts);'
-            )
-        }
-        # @formatter:on
-
-        suffix = ("_{0:0" + str(len(str(self.n_estimators))) + "d}")
-        for idx, model in enumerate(self.models):
-            fn_name = self.method_name + suffix.format(idx)
-            fn_name = template[self.language].format(idx, self.class_name, fn_name)
-            fn_names.append(fn_name)
-
-        # Generate related trees:
-        fns = []
-        for idx, model in enumerate(self.models):
-            tree = self.create_tree(idx, model)
-            fns.append(tree)
-
-        # @formatter:off
-        template = {
-            'java': (
+            'method': (
                 '{0}'
                 'public static int {1}(float[] atts) {{ \n'
                 '    int n_estimators = {2}; \n'
@@ -275,7 +83,38 @@ class AdaBoostClassifier(Classifier):
                 '    return idx; \n'
                 '}}'
             ),
-            'js': (
+            'class': (
+                'class {0} {{ \n'
+                '    {2} \n'
+                '    public static void main(String[] args) {{ \n'
+                '        if (args.length == {3}) {{ \n'
+                '            float[] atts = new float[args.length]; \n'
+                '            for (int i = 0, l = args.length; i < l; i++) {{ \n'
+                '                atts[i] = Float.parseFloat(args[i]); \n'
+                '            }} \n'
+                '            System.out.println({0}.{1}(atts)); \n'
+                '        }} \n'
+                '    }} \n'
+                '}}'
+            )
+        },
+        'js': {
+            'if':       ('{0}if (atts[{1}] <= {2}) {{'),
+            'else':     ('{0}}} else {{'),
+            'endif':    ('{0}}}'),
+            'arr':      ('{0}classes[{1}] = {2}'),
+            'join':     ('; '),
+            'single_method': (
+                'var {1}_{0} = function(atts) {{ \n'
+                '    var classes = new Array({2});'
+                '    {3} \n\n'
+                '    return classes; \n'
+                '}};'
+            ),
+            'method_calls': (
+                'preds[{0}] = {2}(atts);'
+            ),
+            'method': (
                 '{0} \n'
                 'var {1} = function(atts) {{ \n'
                 '    var n_estimators = {2}, \n'
@@ -326,21 +165,178 @@ class AdaBoostClassifier(Classifier):
                 '    }} \n'
                 '    return idx; \n'
                 '}};'
-            )
+            ),
+            'class': ('{2}')
         }
-        # @formatter:on
+    }
+    # @formatter:on
+
+
+    def __init__(self, language='java', method_name='predict', class_name='Tmp'):
+        super(self.__class__, self).__init__(language, method_name, class_name)
+
+
+    def port(self, model):
+        """Port a trained model to the syntax of a chosen programming language.
+
+        Parameters
+        ----------
+        :param model : AdaBoostClassifier
+            An instance of a trained AdaBoostClassifier model.
+        """
+
+        # Check the used algorithm type:
+        if model.algorithm not in ('SAMME.R'):
+            msg = "The classifier doesn't support the given algorithm %s."
+            raise ValueError(msg, model.algorithm)
+
+        # Check type of base estimators:
+        if not isinstance(model.base_estimator, sklearn.tree.tree.DecisionTreeClassifier):
+            msg = "The classifier doesn't support the given base estimator %s."
+            raise ValueError(msg, model.base_estimator)
+
+        # Check number of base estimators:
+        if not model.n_estimators > 0:
+            msg = "The classifier hasn't any base estimators."
+            raise ValueError(msg)
+
+        self.model = model
+        self.n_classes = model.n_classes_
+
+        self.models = []
+        self.weights = []
+        self.n_estimators = 0
+        for idx in range(self.model.n_estimators):
+            weight = self.model.estimator_weights_[idx]
+            if weight > 0:
+                self.models.append(self.model.estimators_[idx])
+                self.weights.append(self.model.estimator_weights_[idx])
+                self.n_estimators += 1
+                self.n_features = self.model.estimators_[idx].n_features_
+
+        if self.method_name == 'predict':
+            return self.predict()
+
+
+    def predict(self):
+        """Port the predict method.
+
+        Returns
+        -------
+        :return: out : string
+            The ported predict method.
+        """
+        return self.create_class(self.create_method())
+
+
+    def create_branches(self, L, R, T, value, features, node, depth):
+        """Port the structure of the model.
+
+        Parameters
+        ----------
+        :param L : object
+            The left children node.
+        :param R : object
+            The left children node.
+        :param T : object
+            The decision threshold.
+        :param value : object
+            The label or class.
+        :param features : object
+            The feature values.
+        :param node : int
+            The current node.
+        :param depth : int
+            The tree depth.
+
+        Returns
+        -------
+        :return : string
+            The ported structure of the tree model.
+        """
+        str = ''
+        ind = '\n' + '    ' * depth
+        if T[node] != -2.:
+            str += self.temp('if').format(ind, features[node], repr(T[node]))
+            if L[node] != -1.:
+                str += self.create_branches(L, R, T, value, features, L[node], depth + 1)
+            str += self.temp('else').format(ind)
+            if R[node] != -1.:
+                str += self.create_branches(L, R, T, value, features, R[node], depth + 1)
+            str += self.temp('endif').format(ind)
+        else:
+            classes = []
+            for class_idx, val in enumerate(value[node][0]):
+                classes.append(self.temp('arr').format(ind, class_idx, repr(val)))
+            str += self.temp('join').join(classes) + self.temp('join')
+        return str
+
+
+    def create_single_method(self, model_index, model):
+        """Port a method for a single tree.
+
+        Parameters
+        ----------
+        :param model_index : int
+            The model index.
+        :param model : DecisionTreeClassifier
+            The model.
+
+        Returns
+        -------
+        :return : string
+            The created method.
+        """
+        feature_indices = []
+        for idx in model.tree_.feature:
+            feature_indices.append([str(jdx) for jdx in range(model.n_features_)][idx])
+
+        tree_branches = self.create_branches(
+            model.tree_.children_left,
+            model.tree_.children_right,
+            model.tree_.threshold,
+            model.tree_.value,
+            feature_indices, 0, 1)
+
+        return self.temp('single_method').format(
+            str(model_index),
+            self.method_name,
+            self.n_classes,
+            tree_branches)
+
+
+    def create_method(self):
+        """Build the model methods or functions.
+
+        Returns
+        -------
+        :return out : string
+            The built methods as merged string.
+        """
+        # Generate method or function names:
+        fn_names = []
+        suffix = ("_{0:0" + str(len(str(self.n_estimators))) + "d}")
+        for idx, model in enumerate(self.models):
+            fn_name = self.method_name + suffix.format(idx)
+            fn_name = self.temp('method_calls').format(idx, self.class_name, fn_name)
+            fn_names.append(fn_name)
+
+        # Generate related trees:
+        fns = []
+        for idx, model in enumerate(self.models):
+            tree = self.create_single_method(idx, model)
+            fns.append(tree)
 
         # Merge generated content:
-        out = template[self.language].format(
+        return self.temp('method').format(
             '\n'.join(fns),
             self.method_name,
             self.n_estimators,
             self.n_classes,
             '\n'.join(fn_names))
-        return out
 
 
-    def create_class(self):
+    def create_class(self, method):
         """Build the model class.
 
         Returns
@@ -348,28 +344,8 @@ class AdaBoostClassifier(Classifier):
         :return out : string
             The built class as string.
         """
-        # @formatter:off
-        template = {
-            'java': (
-                'class {0} {{{{ \n'
-                '    {{0}} \n'
-                '    public static void main(String[] args) {{{{ \n'
-                '        if (args.length == {2}) {{{{ \n'
-                '            float[] atts = new float[args.length]; \n'
-                '            for (int i = 0, l = args.length; i < l; i++) {{{{ \n'
-                '                atts[i] = Float.parseFloat(args[i]); \n'
-                '            }}}} \n'
-                '            System.out.println({0}.{1}(atts)); \n'
-                '        }}}} \n'
-                '    }}}} \n'
-                '}}}}'
-            ),
-            'js': ('{{0}}')
-        }
-        # @formatter:off
-
-        out = template[self.language].format(
+        return self.temp('class').format(
             self.class_name,
             self.method_name,
+            method,
             self.n_features)
-        return out
