b7de0dddf746badee222aa900ddd68f873d0cf76
Darius Morawiec <darius.morawiec@nok.onl>
2016-10-10 20:25:31 +0200

    cleanup tests

--- tests/classifier/AdaBoostClassifierTest.py
+++ tests/classifier/AdaBoostClassifierTest.py
@@ -1,31 +1,33 @@
 import random
-import subprocess
+import subprocess as subp
 import unittest
 import numpy as np
 
 from sklearn.datasets import load_iris
-from sklearn.ensemble import AdaBoostClassifier as ADA
-from sklearn.tree import DecisionTreeClassifier as DT
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
-from onl.nok.sklearn.classifier.AdaBoostClassifier import AdaBoostClassifier
+from onl.nok.sklearn.classifier.AdaBoostClassifier import AdaBoostClassifier as Porter
 
 
 class AdaBoostClassifierTest(unittest.TestCase):
 
-    TESTS = 150
+    N_TESTS = 150
 
     def setUp(self):
         self.tmp_fn = 'Tmp'
         self.iris = load_iris()
         self.n_features = len(self.iris.data[0])
-        base_estimator = DT(max_depth=4, random_state=0)
-        self.clf = ADA(base_estimator=base_estimator, n_estimators=100, random_state=0)
+        base_estimator = DecisionTreeClassifier(max_depth=4,
+                                                random_state=0)
+        self.clf = AdaBoostClassifier(base_estimator=base_estimator,
+                                      n_estimators=100, random_state=0)
         self.clf.fit(self.iris.data, self.iris.target)
+        self.porter = Porter()
 
     def tearDown(self):
         self.clf = None
 
-    # @unittest.expectedFailure
     def test_random_features(self):
         self._create_java_files()
         preds_from_java = []
@@ -34,27 +36,12 @@ class AdaBoostClassifierTest(unittest.TestCase):
         # Creating random features:
         min_vals = np.amin(self.iris.data, axis=0)
         max_vals = np.amax(self.iris.data, axis=0)
-        for features in range(self.TESTS):
+        for features in range(self.N_TESTS):
             features = [random.uniform(min_vals[f], max_vals[f]) for f in range(self.n_features)]
             preds_from_java.append(self._make_prediction_in_java(features))
             preds_from_py.append(self._make_prediction_in_py(features))
 
-        # Debugging: Compare results manually
-        # for i, el in enumerate(preds_from_py):
-        #     print("%d - %d - %d - %s" % (
-        #         i,
-        #         preds_from_py[i],
-        #         preds_from_java[i],
-        #         str(preds_from_py[i] == preds_from_java[i])))
-
-        # errors = 0
-        # for i, el in enumerate(preds_from_py):
-        #     if (preds_from_py[i] != preds_from_java[i]):
-        #         errors += 1
-        # print('Rounding precision error: %f %%' % (float(errors) / float(self.TESTS) * 100.0))
-
         self._remove_java_files()
-        # self.assertTrue(errors < 15)
         self.assertListEqual(preds_from_py, preds_from_java)
 
     def test_existing_features(self):
@@ -66,34 +53,25 @@ class AdaBoostClassifierTest(unittest.TestCase):
             preds_from_java.append(self._make_prediction_in_java(features))
             preds_from_py.append(self._make_prediction_in_py(features))
 
-        # Debugging: Compare results manually
-        # for i, el in enumerate(preds_from_py):
-        #     print("%d - %d - %d - %s" % (
-        #         i,
-        #         preds_from_py[i],
-        #         preds_from_java[i],
-        #         str(preds_from_py[i] == preds_from_java[i])))
-
         self._remove_java_files()
         self.assertListEqual(preds_from_py, preds_from_java)
 
     def _create_java_files(self):
         # rm -rf temp
-        subprocess.call(['rm', '-rf', 'temp'])
+        subp.call(['rm', '-rf', 'temp'])
         # mkdir temp
-        subprocess.call(['mkdir', 'temp'])
+        subp.call(['mkdir', 'temp'])
 
         with open('temp/' + self.tmp_fn + '.java', 'w') as file:
-            porter = AdaBoostClassifier()
-            main_src = porter.port(self.clf)
+            main_src = self.porter.port(self.clf)
             file.write(main_src)
 
         # javac temp/Tmp.java
-        subprocess.call(['javac', 'temp/' + self.tmp_fn + '.java'])
+        subp.call(['javac', 'temp/' + self.tmp_fn + '.java'])
 
     def _remove_java_files(self):
         # rm -rf temp
-        subprocess.call(['rm', '-rf', 'temp'])
+        subp.call(['rm', '-rf', 'temp'])
 
     def _make_prediction_in_py(self, features):
         return int(self.clf.predict([features])[0])
@@ -102,6 +80,6 @@ class AdaBoostClassifierTest(unittest.TestCase):
         cmd = ['java', '-classpath', 'temp', self.tmp_fn]
         params = [str(f).strip() for f in features]
         cmd += params
-        prediction = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        prediction = subp.check_output(cmd, stderr=subp.STDOUT)
         return int(prediction)
 
