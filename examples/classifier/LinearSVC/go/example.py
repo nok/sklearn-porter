0b1cf3f242e52783006ab1eba173ee03679d6d25
Darius Morawiec <darius.morawiec@nok.onl>
2016-11-01 16:32:40 +0100

    update porter output

--- examples/classifier/LinearSVC/go/example.py
+++ examples/classifier/LinearSVC/go/example.py
@@ -21,32 +21,32 @@ import (
 
 func predict(atts []float64) int {
 
-	coefs := [][]float64{{0.18424209458473811, 0.45123000025163923, -0.80794587716737576, -0.45071660033253858}, {0.052877455748516447, -0.89214995228605254, 0.40398084459610972, -0.9376821661447452}, {-0.85070784319293802, -0.98670214922204336, 1.381010448739191, 1.8654095662423917}}
-	inters := []float64{0.10956266406702335, 1.6636707776739579, -1.7096109416521363}
+		coefs := [][]float64{{0.18424209458473811, 0.45123000025163923, -0.80794587716737576, -0.45071660033253858}, {0.052877455748516447, -0.89214995228605254, 0.40398084459610972, -0.9376821661447452}, {-0.85070784319293802, -0.98670214922204336, 1.381010448739191, 1.8654095662423917}}
+		inters := []float64{0.10956266406702335, 1.6636707776739579, -1.7096109416521363}
 
-	if len(atts) != len(coefs[0]) {
-		return -1
-	}
+		if len(atts) != len(coefs[0]) {
+			return -1
+		}
 
-	classIdx := -1
-	classVal := math.Inf(-1)
-	outerCount, innerCount := len(coefs), len(coefs[0])
-	for i := 0; i < outerCount; i++ {
-		var prob float64
-		for j := 0; j < innerCount; j++ {
-			prob = prob + coefs[i][j]*atts[j]
+		classIdx := -1
+		classVal := math.Inf(-1)
+		outerCount, innerCount := len(coefs), len(coefs[0])
+		for i := 0; i < outerCount; i++ {
+			var prob float64
+			for j := 0; j < innerCount; j++ {
+				prob = prob + coefs[i][j]*atts[j]
+			}
+			if prob+inters[i] > classVal {
+				classVal = prob + inters[i]
+				classIdx = i
+			}
 		}
-		if prob+inters[i] > classVal {
-			classVal = prob + inters[i]
-			classIdx = i
-		}
+		return classIdx
 	}
-	return classIdx
-}
 
 func main() {
-	atts := []float64{1, 2, 3, 4}
-	classIdx := predict(atts)
-	fmt.Println(classIdx)
+	// atts := []float64{ /* values */ }
+	// classIdx := predict(atts)
+	// fmt.Println(classIdx)
 }
 """
