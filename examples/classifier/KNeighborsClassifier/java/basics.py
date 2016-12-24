bd3025b886ea5270c83473a946eeec2e55b31c9e
Darius Morawiec <darius.morawiec@nok.onl>
2016-12-24 11:04:56 +0100

    Fix memory of minimum distance

--- examples/classifier/KNeighborsClassifier/java/basics.py
+++ examples/classifier/KNeighborsClassifier/java/basics.py
@@ -7,8 +7,10 @@ X, y = load_iris(return_X_y=True)
 clf = KNeighborsClassifier(algorithm='brute', n_neighbors=1, weights='distance')
 clf.fit(X, y)
 
+# print(clf.predict([[5.4231313, 3.712313223, 1.5123123, 0.2123123]]))
+
 # Cheese!
 
 result = Porter().port(clf)
 # model = Porter(language='java').port(clf)
-print(result)
+# print(result)
