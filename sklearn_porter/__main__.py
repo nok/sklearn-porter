0e273cc90eeac613e513663ff98aa97b64623a29
Darius Morawiec <darius.morawiec@nok.onl>
2017-02-04 13:20:20 +0100

    Improve code quality

--- sklearn_porter/__main__.py
+++ sklearn_porter/__main__.py
@@ -36,25 +36,30 @@ def main():
 
     model_path = str(args['input'])
     if model_path.endswith('.pkl') and os.path.isfile(model_path):
+
         # Load data:
         from sklearn.externals import joblib
         raw_model = joblib.load(model_path)
+
         # Port model:
         porter = Porter(language=str(args['language']), with_details=True)
         result = porter.port(raw_model)
         filename = result.get('filename')
+
         # Define destination path:
-        if str(args['output']) != '' and os.path.isdir(str(args['output'])):
-            model_path = str(args['output'])
-            model_path = os.path.join(model_path, filename)
+        dest_dir = str(args['output'])
+        if dest_dir != '' and os.path.isdir(dest_dir):
+            dest_dir = str(args['output'])
+            dest_path = os.path.join(dest_dir, filename)
         else:
-            model_path = model_path.split(os.sep)
+            dest_dir = model_path.split(os.sep)
             # if len(model_path) > 1:
-            del model_path[-1]
-            model_path += [filename]
-            model_path = os.sep.join(model_path)
-        # Save ported model:
-        with open(model_path, 'w') as f:
+            del dest_dir[-1]
+            dest_dir += [filename]
+            dest_path = os.sep.join(dest_dir)
+
+        # Save transpiled model:
+        with open(dest_path, 'w') as f:
             f.write(result.get('model'))
     else:
         raise ValueError('No valid model in pickle format was found.')
