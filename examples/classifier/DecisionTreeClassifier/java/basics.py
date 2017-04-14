c9e8885f98a74c8da0b2f9bfac1f05600b4da5f9
Darius Morawiec <darius.morawiec@nok.onl>
2017-04-14 14:34:14 +0200

    Update decision values

--- examples/classifier/DecisionTreeClassifier/java/basics.py
+++ examples/classifier/DecisionTreeClassifier/java/basics.py
@@ -23,59 +23,59 @@ class Brain {
     public static int predict(float[] atts) {
         if (atts.length != 4) { return -1; }
         int[] classes = new int[3];
-
-        if (atts[3] <= 0.80000001192092896) {
-            classes[0] = 50;
-            classes[1] = 0;
-            classes[2] = 0;
+            
+        if (atts[2] <= 2.4500000476837158) {
+            classes[0] = 50; 
+            classes[1] = 0; 
+            classes[2] = 0; 
         } else {
             if (atts[3] <= 1.75) {
                 if (atts[2] <= 4.9499998092651367) {
                     if (atts[3] <= 1.6500000953674316) {
-                        classes[0] = 0;
-                        classes[1] = 47;
-                        classes[2] = 0;
+                        classes[0] = 0; 
+                        classes[1] = 47; 
+                        classes[2] = 0; 
                     } else {
-                        classes[0] = 0;
-                        classes[1] = 0;
-                        classes[2] = 1;
+                        classes[0] = 0; 
+                        classes[1] = 0; 
+                        classes[2] = 1; 
                     }
                 } else {
                     if (atts[3] <= 1.5499999523162842) {
-                        classes[0] = 0;
-                        classes[1] = 0;
-                        classes[2] = 3;
+                        classes[0] = 0; 
+                        classes[1] = 0; 
+                        classes[2] = 3; 
                     } else {
-                        if (atts[0] <= 6.9499998092651367) {
-                            classes[0] = 0;
-                            classes[1] = 2;
-                            classes[2] = 0;
+                        if (atts[2] <= 5.4499998092651367) {
+                            classes[0] = 0; 
+                            classes[1] = 2; 
+                            classes[2] = 0; 
                         } else {
-                            classes[0] = 0;
-                            classes[1] = 0;
-                            classes[2] = 1;
+                            classes[0] = 0; 
+                            classes[1] = 0; 
+                            classes[2] = 1; 
                         }
                     }
                 }
             } else {
                 if (atts[2] <= 4.8500003814697266) {
                     if (atts[0] <= 5.9499998092651367) {
-                        classes[0] = 0;
-                        classes[1] = 1;
-                        classes[2] = 0;
+                        classes[0] = 0; 
+                        classes[1] = 1; 
+                        classes[2] = 0; 
                     } else {
-                        classes[0] = 0;
-                        classes[1] = 0;
-                        classes[2] = 2;
+                        classes[0] = 0; 
+                        classes[1] = 0; 
+                        classes[2] = 2; 
                     }
                 } else {
-                    classes[0] = 0;
-                    classes[1] = 0;
-                    classes[2] = 43;
+                    classes[0] = 0; 
+                    classes[1] = 0; 
+                    classes[2] = 43; 
                 }
             }
         }
-
+    
         int class_idx = 0;
         int class_val = classes[0];
         for (int i = 1; i < 3; i++) {
