2935828735fb1a8141c32f5f772172c12877c42d
Darius Morawiec <darius.morawiec@nok.onl>
2017-08-22 00:32:43 +0200

    Refactor and fix lost requirements.txt file

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 
-path = os.path.abspath(os.path.dirname(__file__))
+def read_version():
+    src_dir = os.path.abspath(os.path.dirname(__file__))
+    ver_file = os.path.join(src_dir, 'sklearn_porter', '__version__.txt')
+    version = open(ver_file, 'r').readlines().pop()
+    if isinstance(version, bytes):
+        version = version.decode('utf-8')
+    version = str(version).strip()
+    return version
 
-# Version:
-version_path = str(os.path.join(path, 'sklearn_porter', '__version__.txt'))
-version = open(version_path).readlines().pop()
-if isinstance(version, bytes):
-    version = version.decode('utf-8')
-version = str(version).strip()
 
-# Requirements:
-requirements_path = os.path.join(path, 'requirements.txt')
-with open(requirements_path) as f:
-    all_reqs = f.read().split('\n')
-requirements = [x.strip() for x in all_reqs if 'git+' not in x]
+def parse_requirements():
+    src_dir = os.path.abspath(os.path.dirname(__file__))
+    req_file = os.path.join(src_dir, 'requirements.txt')
+    reqs = open(req_file, 'r').read().strip().split('\n')
+    reqs = [req.strip() for req in reqs if 'git+' not in req]
+    return reqs
+
 
 setup(
     name='sklearn-porter',
     packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=True,
-    version=version,
+    version=read_version(),
     description='Transpile trained scikit-learn models to C, Java, JavaScript and others.',
     author='Darius Morawiec',
     author_email='ping@nok.onl',
@@ -38,7 +42,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    install_requires=requirements,
+    install_requires=parse_requirements(),
     keywords=['sklearn', 'scikit-learn'],
     license='MIT',
 )
