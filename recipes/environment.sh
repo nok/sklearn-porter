97166c2cdb3da1a3146a274de67ff82939e1b512
Darius Morawiec <darius.morawiec@nok.onl>
2017-09-02 11:29:29 +0200

    Set the channel locally

--- recipes/environment.sh
+++ recipes/environment.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-conda config --add channels conda-forge
-conda env create -n sklearn-porter python=2 -f environment.yml
+conda env create -c conda-forge -n sklearn-porter python=2 -f environment.yml
 source activate sklearn-porter
