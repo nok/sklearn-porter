84b48b9be466ac72bddf5ee6288ff48be26eed62
Darius Morawiec <darius.morawiec@nok.onl>
2017-09-26 19:32:41 +0200

    Reduce the number of trees

--- tests/classifier/RandomForestClassifier/RandomForestClassifierPHPTest.py
+++ tests/classifier/RandomForestClassifier/RandomForestClassifierPHPTest.py
@@ -13,15 +13,7 @@ class RandomForestClassifierPHPTest(PHP, Classifier, TestCase):
 
     def setUp(self):
         super(RandomForestClassifierPHPTest, self).setUp()
-        self.mdl = RandomForestClassifier(n_estimators=100, random_state=0)
+        self.mdl = RandomForestClassifier(n_estimators=20, random_state=0)
 
     def tearDown(self):
         super(RandomForestClassifierPHPTest, self).tearDown()
-
-    @unittest.skip('The generated code would be too large.')
-    def test_existing_features_w_digits_data(self):
-        pass
-
-    @unittest.skip('The generated code would be too large.')
-    def test_random_features_w_digits_data(self):
-        pass
