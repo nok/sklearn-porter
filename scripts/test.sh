81396b2523df09e4d71eb0cdd16855abcb4fa21c
Darius Morawiec <darius.morawiec@nok.onl>
2017-11-25 23:55:20 +0100

    Add start of local server

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 source activate sklearn-porter
+python -m $(python -c 'import sys; print("http.server" if sys.version_info[:2] > (2,7) else "SimpleHTTPServer")') 8080 & serve_pid=$!
 python -m unittest discover -vp '*Test.py'
+kill $serve_pid
 source deactivate
