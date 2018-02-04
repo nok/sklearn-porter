bf26057d8e9e1a6029e3be1a75bc24774f1f17bf
Darius Morawiec <darius.morawiec@nok.onl>
2018-02-04 22:32:44 +0100

    feature/cli-alias: Add (smart) function as alias

---
+++ scripts/alias.sh
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/bin/bash
+
+function porter() {
+  if [[ "$(pip list --format=columns | grep sklearn-porter)" ]]
+  then
+    local CMDS="from distutils.sysconfig import get_python_lib;\
+                print(get_python_lib())"
+    local LIBS=$(python -c "$CMDS")
+    python $LIBS/sklearn_porter "$@"
+  else
+     echo "Cammond not found."
+  fi
+}
