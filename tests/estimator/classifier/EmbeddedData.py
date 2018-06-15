e79acc80d89686eda3bc889fae4b91eb0bb73cdb
Darius Morawiec <darius.morawiec@nok.onl>
2018-06-15 17:19:21 +0200

    release/0.7.0: Refactor generated tests

--- tests/estimator/classifier/EmbeddedData.py
+++ tests/estimator/classifier/EmbeddedData.py
@@ -10,42 +10,36 @@ class EmbeddedData():
         self._port_estimator(embed_data=True)
         amin = np.amin(self.X, axis=0)
         amax = np.amax(self.X, axis=0)
-        preds, ground_truth = [], []
-        for _ in range(self.N_RANDOM_FEATURE_SETS):
-            x = np.random.uniform(amin, amax, self.n_features)
-            preds.append(self.pred_in_custom(x))
-            ground_truth.append(self.pred_in_py(x))
+        shape = (self.N_RANDOM_FEATURE_SETS, self.n_features)
+        X = np.random.uniform(low=amin, high=amax, size=shape)
+        Y_py = self.estimator.predict(X).tolist()
+        Y = [self.pred_in_custom(x) for x in X]
         self._clear_estimator()
-        # noinspection PyUnresolvedReferences
-        self.assertListEqual(preds, ground_truth)
+        self.assertListEqual(Y, Y_py)
 
     def test_random_features__iris_data__embedded(self):
         self.load_iris_data()
         self._port_estimator(embed_data=True)
         amin = np.amin(self.X, axis=0)
         amax = np.amax(self.X, axis=0)
-        preds, ground_truth = [], []
-        for _ in range(self.N_RANDOM_FEATURE_SETS):
-            x = np.random.uniform(amin, amax, self.n_features)
-            preds.append(self.pred_in_custom(x))
-            ground_truth.append(self.pred_in_py(x))
+        shape = (self.N_RANDOM_FEATURE_SETS, self.n_features)
+        X = np.random.uniform(low=amin, high=amax, size=shape)
+        Y_py = self.estimator.predict(X).tolist()
+        Y = [self.pred_in_custom(x) for x in X]
         self._clear_estimator()
-        # noinspection PyUnresolvedReferences
-        self.assertListEqual(preds, ground_truth)
+        self.assertListEqual(Y, Y_py)
 
     def test_random_features__digits_data__embedded(self):
         self.load_digits_data()
         self._port_estimator(embed_data=True)
         amin = np.amin(self.X, axis=0)
         amax = np.amax(self.X, axis=0)
-        preds, ground_truth = [], []
-        for _ in range(self.N_RANDOM_FEATURE_SETS):
-            x = np.random.uniform(amin, amax, self.n_features)
-            preds.append(self.pred_in_custom(x))
-            ground_truth.append(self.pred_in_py(x))
+        shape = (self.N_RANDOM_FEATURE_SETS, self.n_features)
+        X = np.random.uniform(low=amin, high=amax, size=shape)
+        Y_py = self.estimator.predict(X).tolist()
+        Y = [self.pred_in_custom(x) for x in X]
         self._clear_estimator()
-        # noinspection PyUnresolvedReferences
-        self.assertListEqual(preds, ground_truth)
+        self.assertListEqual(Y, Y_py)
 
     def test_existing_features__binary_data__embedded(self):
         self.load_binary_data()
