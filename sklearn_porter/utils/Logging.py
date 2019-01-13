548f30617cad248a68be7207c61b915b78a56f9b
Darius Morawiec <nok@users.noreply.github.com>
2019-01-13 23:13:52 +0100

    release/0.7.0: Add range validation

--- sklearn_porter/utils/Logging.py
+++ sklearn_porter/utils/Logging.py
@@ -11,6 +11,9 @@ class Logging(object):
     @staticmethod
     def get_logger(name, level=0):
         """Setup a logging instance"""
+        level = 0 if not isinstance(level, int) else level
+        level = 0 if level < 0 else level
+        level = 4 if level > 4 else level
         console = logging.StreamHandler()
         level = [logging.NOTSET, logging.ERROR, logging.WARN, logging.INFO,
                  logging.DEBUG][level]
