c427cd370e3ab00a539a78794320343d1ffa22dc
Darius Morawiec <nok@users.noreply.github.com>
2019-01-20 12:29:53 +0100

    release/0.7.1: Add checks whether linked files (file://) exist or not

--- sklearn_porter/__init__.py
+++ sklearn_porter/__init__.py
@@ -2,6 +2,7 @@
 
 from os.path import abspath
 from os.path import dirname
+from os.path import exists
 from os.path import join
 from json import load
 
@@ -23,16 +24,22 @@ def _load_meta(path):
                 str(meta['requirements']).startswith('file://'):
             req_path = str(meta['requirements'])[7:]
             req_path = join(src_dir, req_path)
-            reqs = open(req_path, 'r').read().strip().split('\n')
-            reqs = [req.strip() for req in reqs if 'git+' not in req]
-            meta['requirements'] = reqs
+            if exists(req_path):
+                reqs = open(req_path, 'r').read().strip().split('\n')
+                reqs = [req.strip() for req in reqs if 'git+' not in req]
+                meta['requirements'] = reqs
+            else:
+                meta['requirements'] = ''
 
         if 'long_description' in meta and \
                 str(meta['long_description']).startswith('file://'):
             readme_path = str(meta['long_description'])[7:]
             readme_path = join(src_dir, readme_path)
-            readme = open(readme_path, 'r').read().strip()
-            meta['long_description'] = readme
+            if exists(readme_path):
+                readme = open(readme_path, 'r').read().strip()
+                meta['long_description'] = readme
+            else:
+                meta['long_description'] = ''
 
     return meta
 
