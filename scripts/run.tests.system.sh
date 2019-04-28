31b1bc33d09c25a699ed28c10deadcae0ebaba22
Darius Morawiec <nok@users.noreply.github.com>
2019-04-28 03:08:51 +0200

    feature/docker-test-environment: Remove comments

--- scripts/run.tests.system.sh
+++ scripts/run.tests.system.sh
@@ -3,20 +3,17 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd ${SCRIPT_PATH}/..
 
-# Test cases:
 PYTHON_FUNCTIONS="test_*"
 PYTHON_FILES="*Test.py"
 if [[ "$1" ]]; then
     PYTHON_FILES=${1}
 fi
 
-# Test dependencies:
 if [[ ! -f ./gson.jar ]]; then
     wget http://central.maven.org/maven2/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar
     mv gson-2.8.5.jar gson.jar
 fi
 
-# Tests:
 pytest tests -v -x -p no:doctest \
     -o python_files=${PYTHON_FILES} \
     -o python_functions=${PYTHON_FUNCTIONS}
