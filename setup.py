7c252add31d4c41d50176af18dd608fa14b77f5d
Darius Morawiec <nok@users.noreply.github.com>
2019-04-28 21:55:29 +0200

    release/1.0.0: Refactor setup with pypi load

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ from os.path import join
 from json import load
 
 
-def _load_meta(path):
+def load_meta(path):
     """
-    Load meta data about this package from file pypi.json.
+    Load meta information from file pypi.json.
     :param path: The path to pypi.json
-    :return: Dictionary of key value pairs.
+    :return: Dictionary of meta information.
     """
-    with open(path) as f:
+    with open(path, mode='r', encoding='utf-8') as f:
         meta = load(f, encoding='utf-8')
         meta = {k: v.decode('utf-8') if isinstance(v, bytes) else v
                 for k, v in meta.items()}
@@ -25,54 +25,55 @@ def _load_meta(path):
 
         if 'requirements' in meta and \
                 str(meta['requirements']).startswith('file://'):
-            req_path = str(meta['requirements'])[7:]
-            req_path = join(src_dir, req_path)
-            if exists(req_path):
-                reqs = open(req_path, 'r', encoding='utf-8').read().strip().split('\n')
+            reqs_path = str(meta['requirements'])[7:]
+            reqs_path = join(src_dir, reqs_path)
+            if exists(reqs_path):
+                reqs = open(reqs_path, 'r', encoding='utf-8').read()
+                reqs = reqs.strip().split('\n')
                 reqs = [req.strip() for req in reqs if 'git+' not in req]
                 meta['requirements'] = reqs
-            else:
-                meta['requirements'] = ''
 
         if 'long_description' in meta and \
                 str(meta['long_description']).startswith('file://'):
             readme_path = str(meta['long_description'])[7:]
             readme_path = join(src_dir, readme_path)
             if exists(readme_path):
-                readme = open(readme_path, 'r', encoding='utf-8').read().strip()
+                readme = open(readme_path, 'r', encoding='utf-8').read()
+                readme = readme.strip()
                 meta['long_description'] = readme
-            else:
-                meta['long_description'] = ''
 
     return meta
 
 
-package = join(abspath(dirname(__file__)), 'sklearn_porter', 'pypi.json')
-meta = _load_meta(package)
-
-
-setup(
-    name=meta.get('name'),
-    description=meta.get('description'),
-    long_description=meta.get('long_description'),
-    long_description_content_type='text/markdown',
-    keywords=meta.get('keywords'),
-    url=meta.get('url'),
-    author=meta.get('author'),
-    author_email=meta.get('author_email'),
-    install_requires=meta.get('requirements'),
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'porter = sklearn_porter.cli.__main__:main'
+def main():
+    path = join(abspath(dirname(__file__)), 'sklearn_porter', 'pypi.json')
+    meta = load_meta(path)
+    setup(
+        name=meta.get('name'),
+        description=meta.get('description'),
+        long_description=meta.get('long_description'),
+        long_description_content_type='text/markdown',
+        keywords=meta.get('keywords'),
+        url=meta.get('url'),
+        author=meta.get('author'),
+        author_email=meta.get('author_email'),
+        install_requires=meta.get('requirements'),
+        packages=find_packages(exclude=["tests.*", "tests"]),
+        include_package_data=True,
+        entry_points={
+            'console_scripts': [
+                'porter = sklearn_porter.cli.__main__:main'
+            ],
+        },
+        classifiers=[
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
         ],
-    },
-    classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    version=meta.get('version'),
-    license=meta.get('license'),
-)
+        version=meta.get('version'),
+        license=meta.get('license'),
+    )
+
+
+if __name__ == '__main__':
+    main()
--- sklearn_porter/__init__.py
+++ sklearn_porter/__init__.py
@@ -9,13 +9,13 @@ from json import load
 from sklearn_porter.Porter import Porter
 
 
-def _load_meta(path):
+def load_meta(path):
     """
-    Load meta data about this package from file pypi.json.
+    Load meta information from file pypi.json.
     :param path: The path to pypi.json
-    :return: Dictionary of key value pairs.
+    :return: Dictionary of meta information.
     """
-    with open(path) as f:
+    with open(path, mode='r', encoding='utf-8') as f:
         meta = load(f, encoding='utf-8')
         meta = {k: v.decode('utf-8') if isinstance(v, bytes) else v
                 for k, v in meta.items()}
@@ -24,30 +24,27 @@ def _load_meta(path):
 
         if 'requirements' in meta and \
                 str(meta['requirements']).startswith('file://'):
-            req_path = str(meta['requirements'])[7:]
-            req_path = join(src_dir, req_path)
-            if exists(req_path):
-                reqs = open(req_path, 'r').read().strip().split('\n')
-                reqs = [req.strip() for req in reqs if 'git+' not in req]
+            reqs_path = str(meta['requirements'])[7:]
+            reqs_path = join(src_dir, reqs_path)
+            if exists(reqs_path):
+                reqs = open(reqs_path, 'r', encoding='utf-8').read()
+                reqs = reqs.strip().split('\n')
+                reqs = [req.strip() for req in reqs]
                 meta['requirements'] = reqs
-            else:
-                meta['requirements'] = ''
 
         if 'long_description' in meta and \
                 str(meta['long_description']).startswith('file://'):
             readme_path = str(meta['long_description'])[7:]
             readme_path = join(src_dir, readme_path)
             if exists(readme_path):
-                readme = open(readme_path, 'r').read().strip()
+                readme = open(readme_path, 'r', encoding='utf-8').read()
+                readme = readme.strip()
                 meta['long_description'] = readme
-            else:
-                meta['long_description'] = ''
 
     return meta
 
 
-package = join(abspath(dirname(__file__)), 'pypi.json')
-meta = _load_meta(package)
+meta = load_meta(join(abspath(dirname(__file__)), 'pypi.json'))
 
 __author__ = meta.get('author')
 __email__ = meta.get('author_email')
