08c1d56eb7c003e98895962275b5ef3bad1d603b
Darius Morawiec <nok@users.noreply.github.com>
2019-05-12 22:00:29 +0200

    feature/oop-api-refactoring: Remove the global conda `environments.yml` file and customize the `install.environment.sh` script by setting the lowest dependencies

--- scripts/install.environment.sh
+++ scripts/install.environment.sh
@@ -4,7 +4,8 @@ if ! conda env list | grep sklearn-porter ; then
     SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
     conda env create \
         -n sklearn-porter \
-        -f ${SCRIPT_PATH}/../environment.yml
+        -c defaults \
+        python=3.5  # python 3.5 is the lowest supported python version
     conda run -n sklearn-porter \
         pip install --upgrade pip
 fi
