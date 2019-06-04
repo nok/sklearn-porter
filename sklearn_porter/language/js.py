58dfd415da1761658cf25cdbd647f72c9c7e0021
Darius Morawiec <nok@users.noreply.github.com>
2019-06-04 23:40:19 +0200

    feature/oop-api-refactoring: Adapt JavaScript version of the DecisionTreeClassifier estimator

--- sklearn_porter/language/js.py
+++ sklearn_porter/language/js.py
@@ -14,3 +14,24 @@ CMD_COMPILE = None
 
 # node estimator.js <args>
 CMD_EXECUTE = 'node {dest_dir}' + sep + '{dest_file}'
+
+TEMPLATES = {
+    # if/else condition:
+    'if':           'if ({0} {1} {2}) {{',
+    'else':         '} else {',
+    'endif':        '}',
+
+    # Basics:
+    'indent':       '    ',
+    'join':         '; ',
+    'type':         '{0}',
+
+    # Arrays:
+    'in_brackets':  '[{0}]',
+    'arr[]':        'var {name} = [{values}];',  # var ages = [1, 2];
+    'arr[][]':      'var {name} = [{values}];',
+
+    # Primitive data types:
+    'int':          '',
+    'double':       ''
+}
