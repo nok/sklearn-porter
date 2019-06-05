5769c1130968e00b530d0dc5ab2cafbdb5d9fd91
Darius Morawiec <nok@users.noreply.github.com>
2019-06-05 22:56:07 +0200

    feature/oop-api-refactoring: Adapt PHP version of the DecisionTreeClassifier estimator

--- sklearn_porter/language/php.py
+++ sklearn_porter/language/php.py
@@ -14,3 +14,24 @@ CMD_COMPILE = None
 
 # php -f {} Estimator.php <args>
 CMD_EXECUTE = 'php -f {dest_dir}' + sep + '{dest_file}'
+
+TEMPLATES = {
+    # if/else condition:
+    'if':           'if ({0} {1} {2}) {{',
+    'else':         '} else {',
+    'endif':        '}',
+
+    # Basics:
+    'indent':       '    ',
+    'join':         '; ',
+    'type':         '{0}',
+
+    # Arrays:
+    'in_brackets':  '[{0}]',
+    'arr[]':        '${name} = [{values}];',  # $ages = [1, 2];
+    'arr[][]':      '${name} = [{values}];',
+
+    # Primitive data types:
+    'int':          '',
+    'double':       ''
+}
