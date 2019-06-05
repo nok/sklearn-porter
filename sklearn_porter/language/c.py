9b408c001d70458322b2e30f2288003096d6484d
Darius Morawiec <nok@users.noreply.github.com>
2019-06-05 23:33:27 +0200

    feature/oop-api-refactoring: Adapt C version of the DecisionTreeClassifier estimator

--- sklearn_porter/language/c.py
+++ sklearn_porter/language/c.py
@@ -15,3 +15,24 @@ CMD_COMPILE = 'gcc {src_dir}' + sep + '{src_file} -std=c99 -lm -o {dest_dir}' + 
 
 # tmp/estimator <args>
 CMD_EXECUTE = '{dest_dir}' + sep + '{dest_file}'
+
+TEMPLATES = {
+    # if/else condition:
+    'if':           'if ({0} {1} {2}) {{',
+    'else':         '} else {',
+    'endif':        '}',
+
+    # Basics:
+    'indent':       '    ',
+    'join':         '; ',
+    'type':         '{0}',
+
+    # Arrays:
+    'in_brackets':  '{{{0}}}',
+    'arr[]':        '{type} {name}[{n}] = {{{values}}};',  # in ages[2] = {1, 2};
+    'arr[][]':      '{type} {name}[{n}][{m}] = {{{values}}};',
+
+    # Primitive data types:
+    'int':          'int',
+    'double':       'double'
+}
