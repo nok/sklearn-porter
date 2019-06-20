292e40547d543a0149b1fcd7e11e314899f9aa6a
Darius Morawiec <nok@users.noreply.github.com>
2019-06-20 11:54:47 +0200

    feature/oop-api-refactoring: Make exceptions simpler and more compact

--- sklearn_porter/exceptions/__init__.py
+++ sklearn_porter/exceptions/__init__.py
@@ -3,51 +3,41 @@
 from sklearn_porter.enums import Language, Method, Template
 
 
-class SklearnPorterError(Exception):
+class NotFittedEstimatorError(Exception):
     def __init__(self, message: str):
-        self.message = message
-        super().__init__(message)
-
-
-class NotFittedEstimatorError(SklearnPorterError):
-    def __init__(self, message: str):
-        hint = 'The passed estimator of kind `{}` ' \
-               'is not fitted.'.format(message)
-        self.message = hint
+        self.message = 'The passed estimator of kind `{}` ' \
+                       'is not fitted.'.format(message)
         super().__init__(self.message)
 
 
-class NotSupportedError(SklearnPorterError):
+class NotSupportedYetError(Exception):
     def __init__(self, message: str):
         hint = 'You can check the documentation ' \
                'or create a new feature request at ' \
                'https://github.com/nok/sklearn-porter .'
-        self.message = message + '\n' + hint
+        self.message = message + '\n\n' + hint
         super().__init__(self.message)
 
 
-class InvalidMethodError(SklearnPorterError):
+class InvalidMethodError(Exception):
     def __init__(self, message: str):
         opts = ', '.join(['`{}`'.format(m.value) for m in list(Method)])
-        hint = 'The passed method `{}` is invalid. ' \
-               'Valid methods are: {}.'.format(message, opts)
-        self.message = hint
+        self.message = 'The passed method `{}` is invalid. ' \
+                       'Valid methods are: {}.'.format(message, opts)
         super().__init__(self.message)
 
 
-class InvalidLanguageError(SklearnPorterError):
+class InvalidLanguageError(Exception):
     def __init__(self, message: str):
         opts = ', '.join(['`{}`'.format(l.value.KEY) for l in list(Language)])
-        hint = 'The passed language `{}` is invalid. ' \
-               'Valid languages are: {}.'.format(message, opts)
-        self.message = hint
+        self.message = 'The passed language `{}` is invalid. ' \
+                       'Valid languages are: {}.'.format(message, opts)
         super().__init__(self.message)
 
 
-class InvalidTemplateError(SklearnPorterError):
+class InvalidTemplateError(Exception):
     def __init__(self, message: str):
         opts = ', '.join(['`{}`'.format(t.value) for t in list(Template)])
-        hint = 'The passed template `{}` is invalid. ' \
-               'Valid templates are: {}.'.format(message, opts)
-        self.message = hint
+        self.message = 'The passed template `{}` is invalid. ' \
+                       'Valid templates are: {}.'.format(message, opts)
         super().__init__(self.message)
