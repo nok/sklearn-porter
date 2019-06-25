f0dacc21151449fff6b89e4e72d4769f506eb101
Darius Morawiec <nok@users.noreply.github.com>
2019-06-25 14:54:12 +0200

    feature/oop-api-refactoring: Adapt the GaussianNBClass estimator

--- sklearn_porter/estimator/GaussianNB/__init__.py
+++ sklearn_porter/estimator/GaussianNB/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from typing import Union, Tuple, Optional
+from json import encoder, dumps
 from copy import deepcopy
 
 from sklearn.naive_bayes import GaussianNB as GaussianNBClass
@@ -20,8 +21,17 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
 
     See also
     --------
-    http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html
+    https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html
     """
+    DEFAULT_LANGUAGE = Language.JAVA
+    DEFAULT_METHOD = Method.PREDICT
+    DEFAULT_TEMPLATE = Template.ATTACHED
+
+    SUPPORT = {
+        Language.JAVA: {Method.PREDICT: {Template.ATTACHED, Template.EXPORTED}},
+        Language.JS: {Method.PREDICT: {Template.ATTACHED, }},
+    }
+
     estimator = None  # type: GaussianNBClass
 
     def __init__(self, estimator: GaussianNBClass):
@@ -29,8 +39,15 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
         L.info('Create specific estimator `%s`.', self.estimator_name)
         est = self.estimator  # alias
 
-        self.meta_info = dict()
-        self.model_data = dict()
+        self.meta_info = dict(
+            n_features=len(est.sigma_[0]),
+            n_classes=len(est.classes_),
+        )
+        self.model_data = dict(
+            priors=est.class_prior_.tolist(),
+            sigmas=est.sigma_.tolist(),
+            thetas=est.theta_.tolist(),
+        )
 
     def port(
             self,
@@ -72,6 +89,62 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
         plas.update(self.meta_info)
 
         # Load templates:
-        temps = self._load_templates(language.value.KEY)
+        tpls = self._load_templates(language.value.KEY)
 
-        return str(self.estimator)
+        # Export template:
+        if language == Language.JAVA and method == Template.EXPORTED:
+            output = str(tpls.get('exported.class').format(**plas))
+            converter = kwargs.get('converter')
+            encoder.FLOAT_REPR = lambda o: converter(o)
+            model_data = dumps(self.model_data, separators=(',', ':'))
+            return output, model_data
+
+        # Pick templates:
+        tpl_double = tpls.get('double')
+        tpl_arr_1 = tpls.get('arr[]')
+        tpl_arr_2 = tpls.get('arr[][]')
+        tpl_in_brackets = tpls.get('in_brackets')
+
+        priors_val = self.model_data.get('priors')
+        priors_val_converted = list(map(converter, priors_val))
+        priors_str = tpl_arr_1.format(
+            type=tpl_double,
+            name='priors',
+            values=', '.join(priors_val_converted)
+        )
+
+        sigmas_val = self.model_data.get('sigmas')
+        sigmas_str = tpl_arr_2.format(
+            type=tpl_double,
+            name='sigmas',
+            values=', '.join(list(tpl_in_brackets.format(', '.join(
+                list(map(converter, v)))) for v in sigmas_val)),
+            n=len(sigmas_val),
+            m=len(sigmas_val[0])
+        )
+
+        thetas_val = self.model_data.get('thetas')
+        thetas_str = tpl_arr_2.format(
+            type=tpl_double,
+            name='thetas',
+            values=', '.join(list(tpl_in_brackets.format(', '.join(
+                list(map(converter, v)))) for v in thetas_val)),
+            n=len(thetas_val),
+            m=len(thetas_val[0])
+        )
+
+        plas.update(dict(
+            priors=priors_str,
+            sigmas=sigmas_str,
+            thetas=thetas_str,
+        ))
+
+        tpl_method = tpls.get('attached.method.predict')
+        out_method = tpl_method.format(**plas)
+
+        plas.update(dict(method=out_method))
+
+        tpl_class = tpls.get('attached.class')
+        out_class = tpl_class.format(**plas)
+
+        return out_class
