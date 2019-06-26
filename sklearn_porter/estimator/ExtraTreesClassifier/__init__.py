475631fe6e0bc41e5c01b7e228c11dab8a6168ba
Darius Morawiec <nok@users.noreply.github.com>
2019-06-26 14:36:59 +0200

    feature/oop-api-refactoring: Adapt the ExtraTreesClassifier estimator

--- sklearn_porter/estimator/ExtraTreesClassifier/__init__.py
+++ sklearn_porter/estimator/ExtraTreesClassifier/__init__.py
@@ -1,73 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from typing import Union, Tuple, Optional
-from copy import deepcopy
+from sklearn.ensemble import ExtraTreesClassifier as ExtraTreesClassifierClass
 
-from sklearn.ensemble.forest import ExtraTreesClassifier \
-    as ExtraTreesClassifierClass
-
-from sklearn_porter.estimator.EstimatorApiABC import EstimatorApiABC
 from sklearn_porter.estimator.EstimatorBase import EstimatorBase
-from sklearn_porter.enums import Method, Language, Template
+from sklearn_porter.estimator.RandomForestClassifier import \
+    RandomForestClassifier
 from sklearn_porter.utils import get_logger
 
 
 L = get_logger(__name__)
 
 
-class ExtraTreesClassifier(EstimatorBase, EstimatorApiABC):
+class ExtraTreesClassifier(RandomForestClassifier, EstimatorBase):
     """Extract model data and port an ExtraTreesClassifier classifier."""
 
     estimator = None  # type: ExtraTreesClassifierClass
 
     def __init__(self, estimator: ExtraTreesClassifierClass):
         super().__init__(estimator)
-        L.info('Create specific estimator `%s`.', self.estimator_name)
-        est = self.estimator  # alias
-
-        self.meta_info = dict()
-        self.model_data = dict()
-
-    def port(
-            self,
-            method: Optional[Method] = None,
-            language: Optional[Language] = None,
-            template: Optional[Template] = None,
-            **kwargs
-    ) -> Union[str, Tuple[str, str]]:
-        """
-        Port an estimator.
-
-        Parameters
-        ----------
-        method : Method
-            The required method.
-        language : Language
-            The required language.
-        template : Template
-            The required template.
-        kwargs
-
-        Returns
-        -------
-        The ported estimator.
-        """
-        method, language, template = self.check(
-            method=method, language=language, template=template)
-
-        # Arguments:
-        kwargs.setdefault('method_name', method.value)
-        converter = kwargs.get('converter')
-
-        # Placeholders:
-        plas = deepcopy(self.placeholders)  # alias
-        plas.update(dict(
-            class_name=kwargs.get('class_name'),
-            method_name=kwargs.get('method_name'),
-        ))
-        plas.update(self.meta_info)
-
-        # Templates:
-        temps = self._load_templates(language.value.KEY)
-
-        return str(self.estimator)
