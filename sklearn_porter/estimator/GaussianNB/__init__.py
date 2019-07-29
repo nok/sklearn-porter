fd97f06d743d7d73b74a1961c4ef4dc1e4e76630
Darius Morawiec <nok@users.noreply.github.com>
2019-07-29 21:09:39 +0200

    feature/oop-api-refactoring: Adapt estimator `GaussianNB` to the new templates

--- sklearn_porter/estimator/GaussianNB/__init__.py
+++ sklearn_porter/estimator/GaussianNB/__init__.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from typing import Union, Tuple, Optional
-from json import encoder, dumps
 from copy import deepcopy
+from json import dumps, encoder
 from logging import DEBUG
+from typing import Optional, Tuple, Union
 
+# scikit-learn
 from sklearn.naive_bayes import GaussianNB as GaussianNBClass
+from sklearn_porter.enums import Language, Method, Template
 
+# sklearn-porter
 from sklearn_porter.estimator.EstimatorApiABC import EstimatorApiABC
 from sklearn_porter.estimator.EstimatorBase import EstimatorBase
-from sklearn_porter.enums import Method, Language, Template
 from sklearn_porter.exceptions import NotFittedEstimatorError
 from sklearn_porter.utils import get_logger
-
 
 L = get_logger(__name__)
 
@@ -26,11 +27,17 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
 
     SUPPORT = {
         Language.JAVA: {
-            Template.ATTACHED: {Method.PREDICT, },
-            Template.EXPORTED: {Method.PREDICT, },
+            Template.ATTACHED: {
+                Method.PREDICT,
+            },
+            Template.EXPORTED: {
+                Method.PREDICT,
+            },
         },
         Language.JS: {
-            Template.ATTACHED: {Method.PREDICT, },
+            Template.ATTACHED: {
+                Method.PREDICT,
+            },
         },
     }
 
@@ -51,8 +58,7 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
             n_features=len(est.sigma_[0]),
             n_classes=len(est.classes_),
         )
-        L.info('Meta info (keys): {}'.format(
-            self.meta_info.keys()))
+        L.info('Meta info (keys): {}'.format(self.meta_info.keys()))
         if L.isEnabledFor(DEBUG):
             L.debug('Meta info: {}'.format(self.meta_info))
 
@@ -61,29 +67,28 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
             sigmas=est.sigma_.tolist(),
             thetas=est.theta_.tolist(),
         )
-        L.info('Model data (keys): {}'.format(
-            self.model_data.keys()))
+        L.info('Model data (keys): {}'.format(self.model_data.keys()))
         if L.isEnabledFor(DEBUG):
             L.debug('Model data: {}'.format(self.model_data))
 
     def port(
-            self,
-            method: Optional[Method] = None,
-            language: Optional[Language] = None,
-            template: Optional[Template] = None,
-            **kwargs
+        self,
+        language: Optional[Language] = None,
+        template: Optional[Template] = None,
+        to_json: bool = False,
+        **kwargs
     ) -> Union[str, Tuple[str, str]]:
         """
         Port an estimator.
 
         Parameters
         ----------
-        method : Method
-            The required method.
         language : Language
             The required language.
         template : Template
             The required template.
+        to_json : bool (default: False)
+            Return the result as JSON string.
         kwargs
 
         Returns
@@ -91,7 +96,8 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
         The ported estimator.
         """
         method, language, template = self.check(
-            method=method, language=language, template=template)
+            language=language, template=template
+        )
 
         # Arguments:
         kwargs.setdefault('method_name', method.value)
@@ -99,10 +105,13 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
 
         # Placeholders:
         plas = deepcopy(self.placeholders)  # alias
-        plas.update(dict(
-            class_name=kwargs.get('class_name'),
-            method_name=kwargs.get('method_name'),
-        ))
+        plas.update(
+            dict(
+                class_name=kwargs.get('class_name'),
+                method_name=kwargs.get('method_name'),
+                to_json=to_json,
+            )
+        )
         plas.update(self.meta_info)
 
         # Templates:
@@ -110,22 +119,22 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
 
         # Export:
         if template == Template.EXPORTED:
-            tpl_class = tpls.get('exported.class')
-            out_class = tpl_class.format(**plas)
+            tpl_class = tpls.get_template('exported.class')
+            out_class = tpl_class.render(**plas)
             converter = kwargs.get('converter')
             encoder.FLOAT_REPR = lambda o: converter(o)
             model_data = dumps(self.model_data, separators=(',', ':'))
             return out_class, model_data
 
         # Pick templates:
-        tpl_double = tpls.get('double')
-        tpl_arr_1 = tpls.get('arr[]')
-        tpl_arr_2 = tpls.get('arr[][]')
-        tpl_in_brackets = tpls.get('in_brackets')
+        tpl_double = tpls.get_template('double').render()
+        tpl_arr_1 = tpls.get_template('arr[]')
+        tpl_arr_2 = tpls.get_template('arr[][]')
+        tpl_in_brackets = tpls.get_template('in_brackets')
 
         priors_val = self.model_data.get('priors')
         priors_val_converted = list(map(converter, priors_val))
-        priors_str = tpl_arr_1.format(
+        priors_str = tpl_arr_1.render(
             type=tpl_double,
             name='priors',
             values=', '.join(priors_val_converted),
@@ -133,37 +142,44 @@ class GaussianNB(EstimatorBase, EstimatorApiABC):
         )
 
         sigmas_val = self.model_data.get('sigmas')
-        sigmas_str = tpl_arr_2.format(
+        sigmas_str = tpl_arr_2.render(
             type=tpl_double,
             name='sigmas',
-            values=', '.join(list(tpl_in_brackets.format(', '.join(
-                list(map(converter, v)))) for v in sigmas_val)),
+            values=', '.join(
+                list(
+                    tpl_in_brackets.render(
+                        value=', '.join(list(map(converter, v)))
+                    ) for v in sigmas_val
+                )
+            ),
             n=len(sigmas_val),
             m=len(sigmas_val[0])
         )
 
         thetas_val = self.model_data.get('thetas')
-        thetas_str = tpl_arr_2.format(
+        thetas_str = tpl_arr_2.render(
             type=tpl_double,
             name='thetas',
-            values=', '.join(list(tpl_in_brackets.format(', '.join(
-                list(map(converter, v)))) for v in thetas_val)),
+            values=', '.join(
+                list(
+                    tpl_in_brackets.render(
+                        value=', '.join(list(map(converter, v)))
+                    ) for v in thetas_val
+                )
+            ),
             n=len(thetas_val),
             m=len(thetas_val[0])
         )
 
-        plas.update(dict(
-            priors=priors_str,
-            sigmas=sigmas_str,
-            thetas=thetas_str,
-        ))
+        plas.update(
+            dict(
+                priors=priors_str,
+                sigmas=sigmas_str,
+                thetas=thetas_str,
+            )
+        )
 
-        tpl_method = tpls.get('attached.method.predict')
-        out_method = tpl_method.format(**plas)
-
-        plas.update(dict(method=out_method))
-
-        tpl_class = tpls.get('attached.class')
-        out_class = tpl_class.format(**plas)
+        tpl_class = tpls.get_template('attached.class')
+        out_class = tpl_class.render(**plas)
 
         return out_class
