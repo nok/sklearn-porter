d711d2e66b03ed932ecc4d288656b1697a4296dc
Darius Morawiec <nok@users.noreply.github.com>
2019-11-18 00:37:48 +0100

    feature/oop-api-refactoring: Add alias decorators

---
+++ sklearn_porter/decorators/__init__.py
@@ -0,0 +1,61 @@
+class alias(object):
+    """
+    Alias class that can be used as a decorator for making methods callable
+    through other names (or "aliases").
+    Note: This decorator must be used inside an @aliased -decorated class.
+    For example, if you want to make the method shout() be also callable as
+    yell() and scream(), you can use alias like this:
+
+        @alias('yell', 'scream')
+        def shout(message):
+            # ....
+
+    Source: http://code.activestate.com/recipes/577659-decorators-for-adding-aliases-to-methods-in-a-clas/
+    """
+    def __init__(self, *aliases):
+        """Constructor."""
+        self.aliases = set(aliases)
+
+    def __call__(self, f):
+        """
+        Method call wrapper. As this decorator has arguments, this method will
+        only be called once as a part of the decoration process, receiving only
+        one argument: the decorated function ('f'). As a result of this kind of
+        decorator, this method must return the callable that will wrap the
+        decorated function.
+        """
+        f._aliases = self.aliases
+        return f
+
+
+def aliased(aliased_class):
+    """
+    Decorator function that *must* be used in combination with @alias
+    decorator. This class will make the magic happen!
+    @aliased classes will have their aliased method (via @alias) actually
+    aliased.
+    This method simply iterates over the member attributes of 'aliased_class'
+    seeking for those which have an '_aliases' attribute and then defines new
+    members in the class using those aliases as mere pointer functions to the
+    original ones.
+
+    Usage:
+        @aliased
+        class MyClass(object):
+            @alias('coolMethod', 'myKinkyMethod')
+            def boring_method():
+                # ...
+
+        i = MyClass()
+        i.coolMethod() # equivalent to i.myKinkyMethod() and i.boring_method()
+
+    Source: http://code.activestate.com/recipes/577659-decorators-for-adding-aliases-to-methods-in-a-clas/
+    """
+    original_methods = aliased_class.__dict__.copy()
+    for name, method in original_methods.items():
+        if hasattr(method, '_aliases'):
+            # Add the aliases for 'method', but don't override any
+            # previously-defined attribute of 'aliased_class'
+            for alias in method._aliases - set(original_methods):
+                setattr(aliased_class, alias, method)
+    return aliased_class
