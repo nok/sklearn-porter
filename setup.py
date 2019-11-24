b0f26b05c75b57ae240c1ab34d7a8fdaa972d133
Darius Morawiec <nok@users.noreply.github.com>
2019-11-24 22:22:42 +0100

    release/1.0.0: Add `joblib` as dependency

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ def main():
             'scikit-learn>=0.17',
             'Jinja2>=2.10.1',
             'loguru>=0.3.2',
+            'joblib',
         ],
         extras_require={
             'examples': ['notebook==5.*'],
