0c53de434c8af53e2a3cdf67bc0ecbcab712a8e7
Darius Morawiec <nok@users.noreply.github.com>
2022-05-22 15:42:56 +0200

    chore: remove redundant helper scripts (main)

--- scripts/clean.build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
-cd ${SCRIPT_PATH}/..
-
-find . -name 'tmp' -maxdepth 1 -type d -delete
-rm -rf build
-rm -rf dist
-rm -rf sklearn_porter.egg-info
--- scripts/install.requirements.examples.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
-
-python -m pip freeze | grep --quiet jupyterlab
-if [[ $? -eq 1 ]]; then
-    cd ${SCRIPT_PATH}/..
-    python -m pip install --no-cache-dir -e ".[examples]"
-fi
